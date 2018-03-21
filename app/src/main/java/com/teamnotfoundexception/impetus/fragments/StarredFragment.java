37d7df70a02799ef3764f6726911e30c8ff7579b
Deepak Chethan <deepakchethan@outlook.com>
2018-03-21 23:45:19 +0000

    Emtpy messages added

--- app/src/main/java/com/teamnotfoundexception/impetus/fragments/StarredFragment.java
+++ app/src/main/java/com/teamnotfoundexception/impetus/fragments/StarredFragment.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.teamnotfoundexception.impetus.Databases.EventsManager;
 import com.teamnotfoundexception.impetus.R;
@@ -43,17 +44,24 @@ public class StarredFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_starred, container, false);
+        RecyclerView recyclerView = (RecyclerView) view;
+        RelativeLayout relativeLayout = (RelativeLayout) view.findViewById(R.id.starHidden);
+        Context context = view.getContext();
+        if (EventsManager.get(context).getEventItemsList().size() == 0){
+            recyclerView.setVisibility(View.GONE);
+            relativeLayout.setVisibility(View.VISIBLE);
+        }else {
+            recyclerView.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+        }
+
         if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
             if (mColumnCount <= 1) {
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
-
             recyclerView.setAdapter(new StarredAdapter(EventsManager.get(context).getEventItemsList(), mListener, getActivity().getApplicationContext()));
-
         }
 
         return view;
